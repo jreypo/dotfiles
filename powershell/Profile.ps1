109ef2dff479bcf5da70c1790825cb6fa70d31d8
Juan Manuel Rey <juanmanuel.reyportal@gmail.com>
2016-02-17 01:49:18 +0100

    Added PowerShell profile

---
+++ powershell/Profile.ps1
@@ -0,0 +1,33 @@
+# Importing Modules #
+
+Import-Module posh-git
+
+#Prompt Function
+
+function shorten-path([string] $path) {
+  $loc = $path.Replace($HOME, '~')
+  # remove prefix for UNC paths
+  $loc = $loc -replace '^[^:]+::', ''
+  # make path shorter like tabs in Vim,
+    # handle paths starting with \\ and . correctly
+  return ($loc -replace '\\(\.?)([^\\]{3})[^\\]*(?=\\)','\$1$2')
+}
+
+function prompt {
+   write-host 'PowerShell' -n -f yellow
+   write-host '-[' -n -f gray
+   write-host (shorten-path (pwd).Path) -n -f white
+   Write-VcsStatus
+   $global:LASTEXITCODE = $realLASTEXITCODE
+   write-host ']' -n -f gray
+   write-host ' %' -n
+   # Update-HostWindowTitle
+   return ' '
+}
+
+# Customized Alias
+
+Set-Alias gep Get-ExecutionPolicy
+Set-Alias sep Set-ExecutionPolicy
+set-alias   more	more.com
+set-alias   for		ForEach-Object
---
+++ powershell/README.md
@@ -0,0 +1,6 @@
+PowerShell Profile
+==========
+
+Requires module [`posh-git`](https://github.com/dahlbyk/posh-git) to be installed. 
+
+Use `Install-Module post-git` to install it if you have [PsGet](http://psget.net/).
