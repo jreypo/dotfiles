2584e380d3e1ff9e7b367111a090dafdcf1f0807
Juan Manuel Rey <juanmanuel.reyportal@gmail.com>
2016-04-06 01:57:59 +0200

    OSX installation script

---
+++ osx/osx_setup.sh
@@ -0,0 +1,260 @@
+#!/usr/bin/env bash
+
+#
+# Author: Juan Manuel Rey
+# Github: https://github.com/jreypo
+# Blog: http://blog.jreypo.io
+#
+
+# Credit: Timo Tsugliani dotfiles (https://github.com/tsugliani/dotfiles.git)
+# Also highly based on ~/.osx — http://mths.be/osx
+
+## Initial configuration
+
+# Ask for the administrator password upfront
+sudo -v
+dotfiles = $HOME/.dotfiles
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Set hostname
+echo -n "Please enter the hostname for this computer: "
+read computername
+sudo scutil --set ComputerName $computername
+sudo scutil --set HostName $computername
+sudo scutil --set LocalHostName $computername
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $computername
+
+## System configuration
+
+echo "Configuring system settings."
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Menu bar: disable transparency
+defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
+
+# Menu bar: show remaining battery percentage; hide time
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+defaults write com.apple.menuextra.battery ShowTime -string "NO"
+
+# Menu bar: hide the useless Time Machine and Volume icons
+defaults write com.apple.systemuiserver menuExtras -array "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" "/System/Library/CoreServices/Menu Extras/AirPort.menu" "/System/Library/CoreServices/Menu Extras/Battery.menu" "/System/Library/CoreServices/Menu Extras/Clock.menu"
+
+# Set sidebar icon size to medium
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 2
+
+# Show scrollbars  only when scrolling
+defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+# Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+## Finder configuration
+
+echo "Configuring Finder."
+
+# Show icons for hard drives, servers, and removable media on the desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Enable spring loading for directories
+defaults write NSGlobalDomain com.apple.springing.enabled -bool true
+
+# Remove the spring loading delay for directories
+defaults write NSGlobalDomain com.apple.springing.delay -float 0
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Automatically open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+## Dock, Dashboard, and hot corners configuration
+
+echo "Configuring Dock and desktop."
+
+# Show indicator lights for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+## Safari configuration
+
+echo "Configuring Safari."
+
+# Set Safari’s home page to `about:blank` for faster loading
+defaults write com.apple.Safari HomePage -string "about:blank"
+
+# Prevent Safari from opening ‘safe’ files automatically after downloading
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+
+# Allow hitting the Backspace key to go to the previous page in history
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true
+
+# Disable Safari’s thumbnail cache for History and Top Sites
+defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+
+# Enable Safari’s debug menu
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+# Make Safari’s search banners default to Contains instead of Starts With
+defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
+
+# Enable the Develop menu and the Web Inspector in Safari
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+
+# Add a context menu item for showing the Web Inspector in web views
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+## Mail.app configuration
+
+echo "Configuring Mail.app."
+
+# Disable send and reply animations in Mail.app
+defaults write com.apple.mail DisableReplyAnimations -bool true
+defaults write com.apple.mail DisableSendAnimations -bool true
+
+# Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+
+# Add the keyboard shortcut ⌘ + Enter to send an email in Mail.app
+defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" -string "@\\U21a9"
+
+# Disable inline attachments (just show the icons)
+defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
+
+## Terminal & iTerm 2 configuration
+echo "Configuring Terminal."
+
+# Only use UTF-8 in Terminal.app
+defaults write com.apple.terminal StringEncodings -array 4
+
+echo "Configuring iTerm 2."
+
+# Install pretty iTerm colors
+open "${dotfiles}/iterm/ahawkins.itermcolors"
+
+# Don’t display the annoying prompt when quitting iTerm
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+
+## Time Machine configuration
+
+echo "Configuring Time Machine."
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Disable local Time Machine backups
+hash tmutil &> /dev/null && sudo tmutil disablelocal
+
+## TextEdit configuration
+
+echo "Configuring TextEdit."
+
+# Use plain text mode for new TextEdit documents
+defaults write com.apple.TextEdit RichText -int 0
+
+# Open and save files as UTF-8 in TextEdit
+defaults write com.apple.TextEdit PlainTextEncoding -int 4
+defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+
+## Install software
+
+echo "Proceeding to install additional software."
+
+# Install Homebrew
+
+echo "Installing Homebrew."
+
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install general packages
+brew update
+brew install asciinema irssi htop coreutils powerline wget xz watch screenfetch dos2unix lftp pandoc tmux reattach-to-user-namespace
+
+# Install dev and sysadmin tools
+brew install git python ansible packer httpie nmap
+
+# Install Atom
+
+echo "Installing Atom."
+
+curl -o Atom.zip -L https://atom.io/download/mac
+unzip Atom.zip
+sudo mv Atom.app /Applications/
+
+# Install Atom apm package manager
+read -p "Please install 'Atom Shell Commands' from Atom menu, and press any key..."
+
+# Install atom packages
+apm install language-powershell language-terraform language-puppet file-icons native-ui idle-theme github-syntax language-awk autocomplete-awk
+brew install homebrew/completions/apm-bash-completion
+
+# Install additional fonts
+git clone https://github.com/abertsch/Menlo-for-Powerline.git $HOME/Downloads/
+sudo cp $HOME/Downloads/Menlo-for-Powerline/*.ttf /Library/Fonts
+
+## Dotfiles setup
+
+echo ".dotfiles setup"
+
+ln -s $dotfiles/vim/vimrc $HOME/.vimrc
+ln -s $dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+ln -s $dotfiles/git/gitconfig $HOME/.gitconfig
+ln -s $dotfiles/git/gitignore_global $HOME/.gitignore_global
+
+## Bash-it setup
+
+echo "Bash-it install and configuration."
+
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+$HOME/.bash_it/install.sh --interactive
+
+mkdir -p $HOME/.bash_it/custom/aliases
+mkdir -p $HOME/.bash_it/custom/themes
+mkdir -p $HOME/.bash_it/custom/themes/modern-jmr
+cp $dotfiles/bash_it/custom.aliases.bash $HOME/.bash_it/custom/aliases/custom.aliases.bash
+cp $dotfiles/bash_it/modern-jmr.theme.bash $HOME/.bash_it/custom/themes/modern-jmr/modern-jmr.theme.bash
+
+## Configuration done
+
+echo "Done. Please restart in order for all the changes to take effect."
