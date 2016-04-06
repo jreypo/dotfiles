c755bf6eb90800ef005bc2564f786cc69fa25b29
Juan Manuel Rey <juanmanuel.reyportal@gmail.com>
2016-04-06 02:07:41 +0200

    Updated OSX bootstrap script

--- osx/osx_setup.sh
+++ osx/osx_setup.sh
@@ -13,7 +13,7 @@
 
 # Ask for the administrator password upfront
 sudo -v
-dotfiles = $HOME/.dotfiles
+dotfiles=$HOME/.dotfiles
 
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
